b376f5a85a2bebf32d7e672d6236d4073151dbdf
Serguei Nastassi <115593821+itoracl@users.noreply.github.com>
2023-03-02 22:04:56 +0300

    Create postgres-cluster-config.tf

---
+++ postgres-cluster-config.tf
@@ -0,0 +1,19 @@
+[
+    {
+        "user" : "user1",
+        "server" : "https://kubernetes.default.svc",
+        "namespace" : "postgres-master",
+        "modules" : [
+            "module1",
+            "module2"
+        ]
+    },
+    {
+        "user" : "user2",
+        "server" : "https://kubernetes.default.svc",
+        "namespace" : "postgres-worker",
+        "modules" : [
+            "module1"
+        ]
+    }
+]
