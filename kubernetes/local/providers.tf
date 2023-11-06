e2121dc2518a0a82c4fe19cca029862eeb276228
Serguei Nastassi <115593821+itoracl@users.noreply.github.com>
2023-11-07 02:56:02 +0300

    Update providers.tf

--- kubernetes/local/providers.tf
+++ kubernetes/local/providers.tf
@@ -1,9 +1,11 @@
 terraform {
     required_providers {
       local = {
+        source = "hashicorp/local"
         version = "~>2.4.0"
       }
       aws = {
+        source = "hashicorp/aws"
         version = "~>5.24.0"
       }
     }
