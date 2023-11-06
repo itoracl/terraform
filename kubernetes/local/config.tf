4dd3c139ad85e1bf87bd8871b64b2e898d5ebca6
Serguei Nastassi <115593821+itoracl@users.noreply.github.com>
2023-11-06 03:31:12 +0300

    Update config.tf

---
+++ kubernetes/local/config.tf
@@ -0,0 +1,16 @@
+terraform {
+  backend "s3" {
+    endpoint   = "http://minioapi.minio-dev:9000"
+    bucket     = "terraform"
+    region     = "default"
+    key        = "kubernetes/local/terraform.tfstate"
+    access_key = "HOIO0bhei0ttkLWI"
+    secret_key = "i9XZ3DxvEL1MzM4W6p6Ze6YtmzTEqMsp"
+
+    force_path_style = true
+    encrypt.   = false
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
