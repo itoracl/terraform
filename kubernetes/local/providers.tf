544d395d30a44096e83cbef2fb75e5a2385fd2a8
Serguei Nastassi <115593821+itoracl@users.noreply.github.com>
2023-11-07 03:39:19 +0300

    Update providers.tf

--- kubernetes/local/providers.tf
+++ kubernetes/local/providers.tf
@@ -12,11 +12,12 @@ terraform {
 }
 
 provider "aws" {
+  skip_requesting_account_id  = true
   region                      = "default"
   skip_credentials_validation = true
-  skip_requesting_account_id  = true
   skip_metadata_api_check     = true
   skip_region_validation      = true
+  skip_get_ec2_platforms      = true
   s3_force_path_style         = true
   access_key                  = "mock_access_key"
   secret_key                  = "mock_secret_key"
