a505d14f856d4d042aa4f75466db2e79c5b1bf1e
Serguei Nastassi <115593821+itoracl@users.noreply.github.com>
2023-11-07 04:01:38 +0300

    Update config.tf

--- kubernetes/local/config.tf
+++ kubernetes/local/config.tf
@@ -10,6 +10,7 @@ terraform {
     use_path_style = true
     encrypt    = false
 
+    skip_credentials_validation = true
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_metadata_api_check = true
